00646636c88e49fa51f25b31bbad5b0d51524db7
Tuan Anh Tran <me@tuananh.org>
2025-09-10 18:34:31 +0700

    ...
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

---
+++ README.md
@@ -0,0 +1,12 @@
+# firecracker-tools
+
+
+## create_rootfs.sh
+
+```sh
+sudo ./create_rootfs.sh
+```
+
+Requires docker.
+
+You should have `firecracker-vm-alpine-rootfs.ext4` in `/tmp` after this.
---
+++ create_rootfs.sh
@@ -0,0 +1,50 @@
+#!/bin/env bash
+set -euxo pipefail
+
+# renovate: datasource=docker depName=alpine extractVersion=^(?<version>[0-9]+(\.[0-9]+)+)$
+alpine_version='3.22.1'
+
+# ref https://github.com/firecracker-microvm/firecracker/blob/main/docs/rootfs-and-kernel-setup.md
+
+umount /tmp/firecracker-vm-alpine-rootfs 2>/dev/null || true
+if [ -e /tmp/firecracker-vm-alpine-rootfs ]; then
+    rmdir /tmp/firecracker-vm-alpine-rootfs
+fi
+rm -f /tmp/firecracker-vm-alpine-rootfs.ext4
+install -m 700 -d /tmp/firecracker-vm-alpine-rootfs
+install -m 600 /dev/null /tmp/firecracker-vm-alpine-rootfs.ext4
+truncate --size 128M /tmp/firecracker-vm-alpine-rootfs.ext4
+mkfs.ext4 -F /tmp/firecracker-vm-alpine-rootfs.ext4
+mount /tmp/firecracker-vm-alpine-rootfs.ext4 /tmp/firecracker-vm-alpine-rootfs
+docker run -i --rm -v /tmp/firecracker-vm-alpine-rootfs:/rootfs "alpine:$alpine_version" <<'EOF'
+set -euxo pipefail
+
+# set the root password.
+echo 'root:root' | chpasswd
+
+# disable the virtual login terminals.
+sed -i -E 's/^(tty\d+:.+)/#\1/g' /etc/inittab
+
+# enable the serial port login terminal.
+sed -i -E 's/^#(ttyS0:.+)/\1/g' /etc/inittab
+
+# install the required packages.
+apk add openrc
+apk add util-linux
+
+# on boot, mount the required filesystems.
+rc-update add devfs boot
+rc-update add procfs boot
+rc-update add sysfs boot
+
+# install into rootfs.
+for d in bin etc lib root sbin usr; do
+    tar c "/$d" | tar x -C /rootfs
+done
+for d in dev proc run sys var; do
+    install -d "/rootfs/$d"
+done
+EOF
+umount /tmp/firecracker-vm-alpine-rootfs
+rmdir /tmp/firecracker-vm-alpine-rootfs
+# dumpe2fs /tmp/firecracker-vm-alpine-rootfs.ext4
